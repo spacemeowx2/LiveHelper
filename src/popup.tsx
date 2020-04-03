472770bcc37c5b9a8956f2e80932949bf1638c73
Licardo <1014660822@qq.com>
2020-04-03 17:18:40 +0800

    细节优化 (#10)
    * 添加网站 icon
    
    * 添加图标素材源文件(sketch 格式)
    
    * 细节优化
    
    * 将site-header 的链接改为 site-name, 防止误点
    
    * 细节调整

--- src/popup.tsx
+++ src/popup.tsx
@@ -68,7 +68,12 @@ const Site: React.FC<{
     window.open(item.info.homepage)
   }, [ item ])
   return <div className='site'>
-    <Localized id={`site-${id}`}><div className='site-header' onClick={handleClick}>{id}</div></Localized>
+    <div className="site-header">
+      <div className="site-name" onClick={handleClick}>
+        <img className="site-icon" alt={id} src={`/src/img/${id}.svg`} />
+        <Localized id={`site-${id}`}>{id}</Localized>
+      </div>
+    </div>
     {
       !item.error ?
         item.living.length === 0 ?
