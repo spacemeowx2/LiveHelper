92619692e07bd58db2c0b5be61ac91da57ed61c0
spacemeowx2 <spacemeowx2@gmail.com>
2020-04-05 00:03:33 +0800

    update build script

--- build/build.js
+++ build/build.js
@@ -1,61 +1,49 @@
-var babel = require('babel-core')
-var fs = require('fs')
-var path = require('path')
-var archiver = require('archiver')
+const Bundler = require('parcel');
+const fs = require('fs')
+const archiver = require('archiver')
+const package = require('../package')
 
-console.log('transform to es5...')
-var file2trans = ['js/subm.js']
-file2trans.forEach(function (filename) {
-  var ret = babel.transformFileSync(filename, {
-    presets: ['es2015']
+process.env.NODE_ENV = 'production';
+const options = {
+  minify: true,
+  cache: true,
+  sourceMaps: true,
+  autoinstall: true,
+  contentHash: false,
+  publicUrl: './',
+};
+
+async function main(){
+  // 同步manifest的版本
+  console.log('sync version...', package.version)
+  var manifest = fs.readFileSync('manifest.json', {
+    encoding: 'utf-8'
   })
-  var newName = filename.replace(/\.js$/, '-es5.js')
-  fs.writeFileSync(newName, ret.code)
-})
+  manifest = manifest.replace(/("version"\s*:\s*)"(\d+\.\d+\.\d+)"/, function (_, v) {
+    return v + '"' + package.version + '"';
+  })
+  fs.writeFileSync('manifest.json', manifest)
 
-
-// 同步manifest的版本
-console.log('sync version...')
-var package = require('../package')
-var manifest = fs.readFileSync('manifest.json', {
-  encoding: 'utf-8'
-})
-manifest = manifest.replace(/("version"\s*:\s*)"(\d+\.\d+\.\d+)"/, function (_, v) {
-  return v + '"' + package.version + '"';
-})
-fs.writeFileSync('manifest.json', manifest)
-
-manifest = JSON.parse(manifest)
-manifest.applications = {
-  gecko: {
-    id: 'spacemeowx2@gmail.com',
-    strict_min_version: '48.0'
-  }
+  const bundler = new Bundler(['src/*.html', 'src/background.ts'], options)
+  await bundler.bundle()
+  bundler.stop()
+  await zip('manifest.json', 'versions/mlh-'+package.version+'.zip')
+  console.log('done')
 }
-fs.writeFileSync('manifest-fx.json', JSON.stringify(manifest, null, 2))
+main().catch(e => console.error(e))
 
-// 压缩成zip
+
 function zip(manifest, filename) {
   console.log('ziping...', manifest)
-  try {fs.mkdirSync('versions')} catch (e) {}
   var archive = archiver.create('zip', {})
   var output = fs.createWriteStream(filename)
-  var zipDirs = ['bootstrap-3.2.0', 'css', 'icon', 'img', 'js', '_locales']
-  var zipFiles = ['background.html', 'options.html', 'popup.html']
 
-  archive.pipe(output)
+  archive
+    .directory('dist')
+    .directory('icon')
+    .directory('_locales')
+    .file(manifest)
+    .pipe(output)
 
-  zipDirs.forEach(function (dir) {
-    archive.directory(dir, dir)
-  })
-  zipFiles.forEach(function (file) {
-    archive.file(file)
-  })
-  archive.file(manifest, {
-    name: 'manifest.json'
-  })
-  archive.finalize()
+  return archive.finalize()
 }
-
-zip('manifest.json', 'versions/mlh-'+package.version+'.zip')
-zip('manifest-fx.json', 'versions/mlh-'+package.version+'.xpi')
