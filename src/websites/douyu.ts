08949911eb446d6711c7507711ff3a68e486a291
spacemeowx2 <spacemeowx2@gmail.com>
2020-08-03 13:55:43 +0800

    feat: filter douyu loop video out

--- src/websites/douyu.ts
+++ src/websites/douyu.ts
@@ -1,41 +1,67 @@
 import { registerWebSite, Living, PollError, PollErrorType } from '../types'
-import { parseHTML, HTMLElement, mapFilter, now } from '~/utils'
+import { mapFilter, parseViews } from '~/utils'
 
-function getInfoFromItem (item: HTMLElement): Living | undefined {
-  if (!item.querySelector('i.icon_live')) {
+interface Item {
+  room_id: number
+  room_src: string
+  vertical_src: string
+  isVertical: number
+  is_special: number
+  cate_id: number
+  child_id: number
+  room_name: string
+  nickname: string
+  avatar_small: string
+  status: number
+  show_status: number
+  show_time: number
+  nrt: number
+  url: string
+  jumpUrl: string
+  game_name: string
+  online: string
+  hasvid: number
+  vurl: string
+  close_notice: string
+  close_notice_ctime: string
+  sub_rt: number
+  rpos: number
+  icon_outing: number
+  videoLoop: number
+}
+
+type Response = {
+  error: 0
+  data: {
+    list: Item[]
+    total: number
+  }
+} | {
+  error: -1
+}
+
+function getInfoFromItem (item: Item): Living | undefined {
+  if (item.videoLoop === 1 || item.show_status !== 1) {
     return
   }
-  let beginTime: number | null
-  let beginTimeStr = item.querySelector('span.glyphicon01_playtime')?.text?.trim() || ''
-  let timeRE = /(\d+)分钟/.exec(beginTimeStr)
-  if (!timeRE) {
-    beginTime = null
-  } else {
-    beginTime = parseInt(timeRE[1])
-    beginTime = now() - beginTime * 60
-  }
-
-  let roomid = item.querySelector('div div a')?.attributes['href']?.replace('/', '');
   return {
-      title: item.querySelector('h1')?.text?.trim(),
-      startAt: beginTime,
-      author: item.querySelector('span.username')?.text?.trim(),
-      online: parseInt(item.querySelector('span.glyphicon01_hot')?.text?.trim() || ''),
-      preview: item.querySelector('img')?.attributes['data-original'],
-      url: 'https://www.douyu.com/' + roomid
+      title: item.room_name,
+      startAt: item.show_time,
+      author: item.nickname,
+      online: parseViews(item.online),
+      preview: item.room_src,
+      url: 'https://www.douyu.com/' + item.room_id
   }
 }
 
 registerWebSite({
   async getLiving () {
-    const r = await fetch('https://www.douyu.com/room/follow')
-    if (r.url.includes('https://passport.douyu.com/member/login')) {
+    const r: Response = await (await fetch('https://www.douyu.com/wgapi/livenc/liveweb/follow/list?sort=0&cid1=0')).json()
+    if (r.error === -1) {
       throw new PollError(PollErrorType.NotLogin)
     }
-    const dom = parseHTML(await r.text())
-    const list = dom.querySelectorAll('.attention ul li') as HTMLElement[]
 
-    return mapFilter(list, getInfoFromItem)
+    return mapFilter(r.data.list, getInfoFromItem)
   },
   id: 'douyu',
   homepage: 'https://www.douyu.com/'
